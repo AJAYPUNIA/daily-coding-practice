55daf509c0a1704e99411db7845c1f280200c6b1
AJAY PUNIA <punia09111508@gmail.com>
2025-06-13 12:39:22 -0400

    Query to find the names of the customers who have never placed orders

---
+++ SQL/SQL55.sql
@@ -0,0 +1,11 @@
+--  SQL query to find the names of customers who have never placed an order.
+
+
+-- SOLUTION
+
+
+SELECT customers.name 
+FROM customers
+LEFT JOIN orders
+ON customers.customer_id = orders.customer_id
+WHERE orders.customer_id IS NULL;
