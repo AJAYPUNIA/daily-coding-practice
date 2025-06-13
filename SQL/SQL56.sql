a85e3619b99460e60337b1ce53d8037951263b0a
AJAY PUNIA <punia09111508@gmail.com>
2025-06-13 12:47:36 -0400

    Query to retrieve the top 2 best selling products by total quantity sold

---
+++ SQL/SQL56.sql
@@ -0,0 +1,13 @@
+-- query to retrieve the top 2 best-selling products by total quantity sold.
+
+
+-- SOLUTION
+
+
+SELECT products.product_name , SUM(orders.quantity) AS total_quantity
+FROM products
+INNER JOIN orders
+ON products.product_id = orders.product_id
+GROUP BY products.product_name
+ORDER BY total_quantity DESC
+LIMIT 2;
