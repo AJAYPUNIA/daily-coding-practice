4ce7c6a4be287a9568edc179128ed353ff835099
AJAY PUNIA <punia09111508@gmail.com>
2025-07-07 10:27:57 -0400

    Query to return the name of the employee who earn more then the average salary in thier department

---
+++ SQL/SQL119.sql
@@ -0,0 +1,13 @@
+-- names of employees who earn more than the average salary of their department.
+
+
+
+-- solution
+
+
+SELECT name ,  salary , department_id 
+FROM Employee e
+WHERE salary > (SELECT AVG(salary) 
+                FROM Employee 
+                WHERE department_id = e.department_id);
+                
