aba12c58477c8a8a7db73b86c17b53f5407974ad
AJAY PUNIA <punia09111508@gmail.com>
2025-09-08 16:50:16 -0400

    fucntion to get the happy number when return 1 , when theire sum of the square of that number

---
+++ python/arrays/leet happy number using set.py
@@ -0,0 +1,25 @@
+# Write an algorithm to determine if a number n is happy.
+
+# A happy number is a number defined by the following process:
+
+# Starting with any positive integer, replace the number by the sum of the squares of its digits.
+# Repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1.
+# Those numbers for which this process ends in 1 are happy.
+# Return true if n is a happy number, and false if not.
+
+
+def isHappy(n):
+    seen = set()
+
+    def get_next(num):
+        total = 0
+        while num > 0:
+            digit = num % 10
+            total += digit * digit
+            num //= 10
+        return total
+    
+    while n != 1 and n not in seen:
+        seen.add(n)
+        n = get_next(n)
+    return n == 1
