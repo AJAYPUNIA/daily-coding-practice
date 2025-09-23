71526ca1cacab90f8c33ab4f2ca30cbd6a341006
AJAY PUNIA <punia09111508@gmail.com>
2025-09-23 10:09:32 -0400

    leet Function to get the perimeter of the islanbd using loop for both column and rows

---
+++ python/arrays/island perimeter leet.py
@@ -0,0 +1,26 @@
+# You are given row x col grid representing a map where grid[i][j] = 1 
+# represents land and grid[i][j] = 0 represents water.
+
+# Grid cells are connected horizontally/vertically (not diagonally). 
+# The grid is completely surrounded by water, and there is exactly 
+# one island (i.e., one or more connected land cells).
+
+# The island doesn't have "lakes", meaning the water inside isn't
+# connected to the water around the island. One cell is a square with side length 1.
+#  The grid is rectangular, width and height don't exceed 100.
+#  Determine the perimeter of the island.
+
+
+def islandperimter(grid):
+    rows , cols = len(grid) , len(grid[0])
+    perimeter = 0
+
+    for r in range(rows):
+        for c in range(cols):
+            if grid[r][c] == 1:
+                perimeter += 4
+                if r > 0 and grid[r-1][c] == 1:
+                    perimeter -= 2
+                if c > 0 and grid[r][c-1] == 1:
+                    perimeter -=2
+    return perimeter
